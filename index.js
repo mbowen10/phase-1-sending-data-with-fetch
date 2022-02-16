df6ce2a777d6388b7539f42b7d566c387e68602e
Michael Bowen <michaelbowen1092@gmail.com>
2022-02-16 11:53:40 -0500

    commit tree 283d80a9269f191c033b560b15567dbeb6ba1b7b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData= function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":userName,
+            "email":userEmail
+        })
+    }).then(response=> response.json())
+    .then(data=>renderId(data))
+    .catch((error)=> {
+        const span=document.createElement("span")
+        span.innerHTML=error.message;
+        document.body.append(span)
+    
+})
+}
+
+const renderId=data=> {
+    const span=document.createElement("span")
+    span.innerHTML=data.id;
+    document.body.append(span)
+}
